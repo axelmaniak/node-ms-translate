39cf1d450084564723ba76cf89176b4fa5ce8e51
Daniela Bravo <therealdanie@protonmail.com>
2021-10-12 22:18:10 -0500

    intial

---
+++ Readme.md
@@ -0,0 +1,29 @@
+# Node JS translator for MS Translate API 🌎
+
+This script is using [Rapid API's Azure Microsoft Translation implementation](https://rapidapi.com/microsoft-azure-org-microsoft-cognitive-services/api/microsoft-translator-text/)
+
+## Usage
+
+Add your Rapid Api Key into `config.js` (See `config.js.dist` for reference)
+
+```sh
+npm run translate -- -w <textToTranslate> -o <language1,language2>
+```
+
+Where
+
+`--w` Specifies the text to translate
+
+`--o` A comma separated list of the expected languages in which the text will be translated to
+
+A simple text list will be displayed with the requested results
+
+## To be done:
+
+- Rapid API doesn't seem to support requesting multiple languages at once, so i'm discovering a way to avoid a single request per language
+
+- Turn this to an executable
+
+- Handle more exceptions and edge cases
+
+- Add unit tests
---
+++ index.js
@@ -0,0 +1,62 @@
+import axios from "axios";
+import minimist from "minimist";
+import config from "./config.js";
+
+// This should work if Rapid API got fixed and many languages could be sent simultaneously
+
+// const toLocaleParams = (locales) => {
+//   try {
+//     return locales
+//       .split(",")
+//       .reduce(
+//         (toLocales, locale) =>
+//           locale ? [...toLocales, `to=${locale}`] : toLocales,
+//         []
+//       )
+//       .join("&");
+//   } catch (e) {
+//     return [];
+//   }
+// };
+
+const translate = async (text, locales) => {
+  const localeArray = locales.split(",");
+
+  if (!localeArray.length) {
+    return console.error(
+      "Please, add locales in comma separated value format (e.g. -o es,pt,it)"
+    );
+  }
+
+  localeArray.forEach(async (locale) => {
+    const response = await getTranslation(text, locale);
+    console.log(`${locale}: `, response?.data?.[0]?.translations?.[0]?.text);
+  });
+};
+
+const getTranslation = async (Text, to) => {
+  const { params, headers, baseUrl } = config;
+
+  try {
+    return await axios.post(baseUrl, [{ Text }], {
+      params: { ...params, to },
+      headers,
+    });
+  } catch (error) {
+    console.log("There was an error requesting your translation:: ", error);
+  }
+};
+
+function init() {
+  const { w: text, o: locales } = minimist(process.argv.slice(2));
+
+  if (!text || !locales) {
+    return console.error(
+      "Please provide a text to translate (--text) and at least a locale (--locales) for translating"
+    );
+  }
+
+  translate(text, locales);
+}
+
+init();
